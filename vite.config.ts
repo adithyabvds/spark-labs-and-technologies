fc08e9f3e412dc8214780b29ddb17e3501426553
Adithya B V <170810966+adithyabvds@users.noreply.github.com>
2026-03-17 13:51:21 +0530

    Configure server and preview allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,17 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: process.env.BASE_PATH || "/",
   plugins: [react()],
+  server: {
+    host: true, // allow external access
+    allowedHosts: [
+      "spark-labs-and-technologies-1.onrender.com"
+    ]
+  },
+  preview: {
+    allowedHosts: [
+      "spark-labs-and-technologies-1.onrender.com"
+    ]
+  },
   build: {
     outDir: "dist",
     emptyOutDir: true,
